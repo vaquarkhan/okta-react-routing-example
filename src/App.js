c6d645f6505fea20904cf0e75e8e5fa11f4582b0
fishbowlsoftware <nik@fishbowlllc.com>
2021-07-15 13:35:10 -0400

    intial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./App.css";
+
+import { Router, Route } from "wouter";
+
+import { Security, LoginCallback } from "@okta/okta-react";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const issuer = process.env.REACT_APP_OKTA_ISSUER;
+const clientId = process.env.REACT_APP_OKTA_CLIENTID;
+const redirect = process.env.REACT_APP_OKTA_APP_BASE_URL + "/implicit/callback";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.oktaAuth = new OktaAuth({
+      issuer: issuer,
+      clientId: clientId,
+      redirectUri: redirect,
+    });
+
+    this.restoreOriginalUri = async (_oktaAuth, originalUri) => {
+      window.location.replace(
+        toRelativeUrl(originalUri || "/", window.location.origin)
+      );
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <Security
+          oktaAuth={this.oktaAuth}
+          restoreOriginalUri={this.restoreOriginalUri}
+        >
+          <Route path="/" component={Home} />
+          <Route path="/implicit/callback" component={LoginCallback} />
+          <Route path="/Profile" component={Profile} />
+        </Security>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import { useOktaAuth } from "@okta/okta-react";
+
+import Header from "../Components/Header";
+
+const Profile = () => {
+  const { authState, oktaAuth } = useOktaAuth();
+
+  if (!authState?.isAuthenticated) {
+    oktaAuth.signInWithRedirect();
+
+    return (
+      <Container>
+        <p>Please wait while we sign you in</p>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Header authState={authState} oktaAuth={oktaAuth}></Header>
+      <h4>Your profile page</h4>
+
+      <p>Welcome to your profile page </p>
+    </Container>
+  );
+};
+export default Profile;
