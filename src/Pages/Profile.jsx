06af2dcdc52ed0b3b10f9189586b61fbd47e2f4b
fishbowlsoftware <nik@fishbowlllc.com>
2021-10-20 09:22:35 -0400

    use authenticate async

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
 
 import { useOktaAuth } from "@okta/okta-react";
@@ -8,24 +8,34 @@ import Header from "../Components/Header";
 const Profile = () => {
   const { authState, oktaAuth } = useOktaAuth();
 
-  if (!authState?.isAuthenticated) {
-    oktaAuth.signInWithRedirect();
+  useEffect(() => {
+    async function authenticate() {
+      if (!authState) return;
 
+      if (!authState.isAuthenticated) {
+        await oktaAuth.signInWithRedirect();
+      }
+    }
+
+    authenticate();
+  }, [authState, oktaAuth]);
+
+  if (!authState?.isAuthenticated) {
     return (
       <Container>
         <p>Please wait while we sign you in</p>
       </Container>
     );
+  } else {
+    return (
+      <Container>
+        <Header authState={authState} oktaAuth={oktaAuth}></Header>
+        <h4>Your profile page</h4>
+
+        <p>Welcome to your profile page </p>
+      </Container>
+    );
   }
-
-  return (
-    <Container>
-      <Header authState={authState} oktaAuth={oktaAuth}></Header>
-      <h4>Your profile page</h4>
-
-      <p>Welcome to your profile page </p>
-    </Container>
-  );
 };
 
 export default Profile;
